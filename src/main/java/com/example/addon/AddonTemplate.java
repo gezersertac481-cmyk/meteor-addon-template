45e38bb582f5c9df490d85fdf77fe6d701e12969
gezersertac481-cmyk <gezersertac481@gmail.com>
2026-02-05 02:51:33 +0300

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -4,6 +4,7 @@ import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.orbit.EventHandler;
@@ -17,11 +18,14 @@ import org.slf4j.LoggerFactory;
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger("ItemTracersAddon");
     public static final Category CATEGORY = new Category("Custom");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example"); // Hata veren eksik satır buydu
 
     @Override
     public void onInitialize() {
-        LOG.info("Addon Initialized!");
-        Modules.get().add(new ItemTracersModul());
+        LOG.info("Initializing Item Tracers Addon");
+
+        // Modülü sisteme ekle
+        Modules.get().add(new ItemTracers());
     }
 
     @Override
@@ -34,27 +38,28 @@ public class AddonTemplate extends MeteorAddon {
         return "com.example.addon";
     }
 
-    public static class ItemTracersModul extends Module {
-        public ItemTracersModul() {
-            super(CATEGORY, "item-tracers-plus", "Follow items with colored lines.");
+    public static class ItemTracers extends Module {
+        public ItemTracers() {
+            super(CATEGORY, "item-tracers-plus", "Eşyaları renkli çizgilerle takip eder.");
         }
 
         @EventHandler
         private void onRender(Render3DEvent event) {
+            if (mc.world == null || mc.player == null) return;
+
             for (Entity entity : mc.world.getEntities()) {
                 if (entity instanceof ItemEntity item) {
-                    Color color = new Color(255, 255, 255, 150); 
-                    var type = item.getStack().getItem();
+                    Color color = new Color(255, 255, 255, 150);
+                    if (item.getStack().getItem() == Items.ANCIENT_DEBRIS) color = new Color(255, 0, 0, 255);
 
-                    if (type == Items.ANCIENT_DEBRIS) color = new Color(255, 0, 0, 255); 
-                    else if (type == Items.OAK_LOG || type == Items.STRIPPED_OAK_LOG) color = new Color(0, 0, 255, 255);
+                    // Kameranın tam pozisyonunu doğru şekilde alıyoruz
+                    Vec3d cameraPos = mc.gameRenderer.getCamera().getPos();
 
-                    Vec3d start = new Vec3d(0, 0, 75)
-                        .rotateX(-(float) Math.toRadians(mc.gameRenderer.getCamera().getPitch()))
-                        .rotateY(-(float) Math.toRadians(mc.gameRenderer.getCamera().getYaw()))
-                        .add(mc.gameRenderer.getCamera().getPos());
-
-                    event.renderer.line(start.x, start.y, start.z, item.getX(), item.getY(), item.getZ(), color);
+                    event.renderer.line(
+                        cameraPos.x, cameraPos.y, cameraPos.z,
+                        item.getX(), item.getY(), item.getZ(),
+                        color
+                    );
                 }
             }
         }
